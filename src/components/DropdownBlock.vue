936c75e20dd8e98fdc0db699a2bff933d486ad7b
VITALIY <vamelcore@gmail.com>
2023-04-16 22:58:07 +0300

    Feat: makup

---
+++ src/components/DropdownBlock.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { Menu, MenuButton, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import CheckIcon from './icons/CheckIcon.vue';
+</script>
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton class="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm">
+        Guest
+        <ChevronDownIcon class="-mr-1 h-5 w-5" aria-hidden="true" />
+      </MenuButton>
+    </div>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems class="absolute right-0 z-10 sm:w-80 origin-top-right rounded-md bg-[#F6EFE6] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <div :class="['bg-[#F6EFE6] hover:bg-[#FFF8EF] px-4 py-3 flex gap-3 cursor-pointer']">
+            <div class="w-6 h-6 flex align-middle">
+            </div>
+            <div>
+              <p class="text-md font-semibold">Guest</p>
+              <p class="text-xs">Default access level, can leave tributes, share media and stories</p>
+            </div>
+          </div>
+          <div :class="['bg-[#FFF8EF] hover:bg-[#FFF8EF] px-4 py-3 flex gap-3 cursor-pointer']">
+            <div class="w-6 h-6 flex align-middle">
+              <CheckIcon></CheckIcon>
+            </div>
+            <div>
+              <p class="text-md font-semibold">Administrator</p>
+              <p class="text-xs">Can control all aspects of the website, including moderating content posted by others and changing website settings</p>
+            </div>
+          </div>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
---
+++ src/components/ItemBlock.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DropdownBlock from './DropdownBlock.vue';
+import MinusIcon from './icons/MinusIcon.vue'
+</script>
+
+<template>
+  <div class="flex justify-between items-center ml-6 my-1.5 gap-2">
+    <div class="flex justify-between items-center h-12 bg-[#F6EFE6] rounded-md grow px-3">
+      <div>
+        <p class="text-base">John Wick</p>
+        <p class="text-xs">example@email.com</p>
+      </div>
+      <div>
+        <DropdownBlock></DropdownBlock>
+      </div>
+    </div>
+    <button class="text-[#D1CAC1] hover:text-red-500">
+      <MinusIcon></MinusIcon>
+    </button>
+  </div>
+</template>
